84b1dd92ac8dbbf908911ba97ec0f9f7fa41969b
OussamaElar <oussama.elar@gmail.com>
2022-01-30 13:35:40 -0500

    deploy

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
 const players = require('./routes/player')
+const path = require('path');
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -17,10 +19,21 @@ mongoose
 // server setup
 app.get('/', (req, res) => res.send('hello word'));
 
-const port = process.env.PORT || 5000;
-
-app.listen(port, () => console.log(`server is up on ${port}`));
 
 // routes 
 
 app.use('/api/players', players);
+
+// deploy
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
+
+//server
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`server is up on ${port}`));
